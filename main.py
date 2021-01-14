185464e1c79f2876c091c14c66ce9be08e6714d0
Abraham Sesay <abrahamsesay96@gmail.com>
2021-01-14 14:37:12 -0600

    add frames and multitext

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 #   and a widget in the window
 
 import tkinter as tk
+from tkinter.constants import FLAT, X
 
 # create window as instnace of the TK class
 window = tk.Tk()
 
-# create a frame
-navBar = tk.Frame()
-navBar.pack()
 
-textBox = tk.Frame()
-textBox.pack()
+# create a frame
+navBar = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=2)
+navBar.pack(fill=X)
+
+textFrame = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=5)
+textFrame.pack(fill=X)
 
 # labels for nav
 newFile = tk.Label(text="New", master=navBar)
@@ -34,5 +36,8 @@ cutFromFile.pack()
 
 pasteOn = tk.Label(text="Paste", master=navBar)
 pasteOn.pack()
+
+# create text widget
+textBox =tk.Text(master=textFrame, bg="#000")
 
 window.mainloop()
